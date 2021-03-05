9d2a0f883e367aae3c087e925005a4ee677561a6
Yaky Refael <yaky.refael@axonius.com>
2021-03-05 09:40:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [searchValue, updateSearch] = useState('ss')
+  const [playlist, updatePlaylist] = useState([
+    {
+      title: "HOSH at Jai Vilas Palace in Gwalior, India for Cercle",
+      url: "https://www.youtube.com/watch?v=z05IO3gWlog&t=1982s"
+    }
+  ]);
+  const addSong = () => {
+    updatePlaylist([...playlist, { title: "", url: searchValue }])
+    updateSearch('')
+  }
+  const removeFirstSong = () => {
+    const reducedList = playlist.splice(1)
+    updatePlaylist(reducedList)
+  }
   return (
     <div className="container">
       <div className="container__list_pannel">
+
         <div className="container__list_pannel__searchbar">
-          <input type="text" />
-          <button>Add</button>
+          <input type="text" value={searchValue} onChange={(e) => updateSearch(e.target.value)} />
+          <button onClick={addSong}>Add</button>
+          <button onClick={removeFirstSong}>Remove</button>
         </div>
+
         <ul className="container__list_pannel__songs">
-          <li>Song 1</li>
+          {playlist.length ?
+            playlist.map((song, i) => <li key={i}>{song.url}</li>) :
+            <li>empty list</li>
+          }
         </ul>
+
       </div>
       <div className="container__video_pannel">container__video_pannel</div>
     </div>
